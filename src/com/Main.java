eec16bdbd6c001fe4fe130ce72dae222e59247eb
Utsav Popli <utsav_popli@hotmail.com>
2015-10-24 14:48:33 -0700

    setting up directories

---
+++ src/com/Main.java
@@ -0,0 +1,18 @@
+
+
+
+package com;
+
+/**
+ * @author utsav
+ * 
+ *	A standalone module for processing and analyzing the data using spark ML
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
